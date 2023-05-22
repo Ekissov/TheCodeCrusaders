f23750715a60bb179be1676c8539cc9ddaf0efef
SANNESPC\Sanne <sharrekilde@gmail.com>
2023-05-22 09:12:00 +0200

    test made public

--- PigegruppenProjektSynsPunkt/ButiksSystem/Models/Costumer.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Models/Costumer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ButiksSystem.Models
 {
-    internal class Costumer
+    public class Costumer
     {
         public int CostumerID { get; set; }
         public string FirstName { get; set; }
