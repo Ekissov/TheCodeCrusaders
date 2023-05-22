681bb932824de765942a88559e4aeb5ba755fc89
Linda_Agnete <lalj52999@edu.ucl.dk>
2023-05-22 10:28:51 +0200

    Jeg har sat CRUD ind i database mapperne

--- PigegruppenProjektSynsPunkt/ButiksSystem/Repository/CostumerDatabaseSQL.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Repository/CostumerDatabaseSQL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,75 @@ namespace ButiksSystem.Repository
 {
     internal class CostumerDatabaseSQL
     {
+        //Vi giver systemet den connection string den skal bruge for at oprette forbindelse til databasen
+        private static SqlConnection connection = new SqlConnection("Insert connection string HERE!");
+
+        //C
+        public static void CreateCustomer(int CustomerID, string FirstName, string LastName, string PhoneNumber, string Email, int PostalCode, string City, string Address)
+        {
+
+            string query = $"INSERT INTO Customer (CustomerID,FirstName,LastName,PhoneNumber,PostalCode,City,Address) " +
+                $"VALUES ({CustomerID.ToString()}, '{FirstName}','{LastName}','{PhoneNumber}','{Email}','{PostalCode.ToString()}','{City}','{Address}')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+        }
+
+
+        //R
+        public static List<string> ReadCustomer()
+        {
+            List<string> result = new List<string>();
+            string query = "SELECT * FROM Customer";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader(); //Åbner readeren 
+
+            while (reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row += reader[i].ToString() + " - "; // Aflæser data og laver det om til string
+                }
+                result.Add(row); //tilføjer data til listen
+            }
+
+            reader.Close(); // lukker aflæseren og forbindelsen 
+            connection.Close();
+            return result; // retunerer resultatet som en liste
+
+        }
+
+        //U
+        public static void UpdateCustomer(int CustomerID, string FirstName, string LastName, string PhoneNumber, string Email, int PostalCode, string City, string Address)
+        {
+            //Update sætningen herunder er muligvis ikke korrekt
+            string query = $"UPDATE Customer SET FirstName = '{FirstName}', LastName = '{LastName}', PhoneNumber = '{PhoneNumber}', Email = '{Email}', PostalCode = '{PostalCode}', City = '{City}', Address = '{Address}' WHERE CustomerID = '{CustomerID}'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+
+        }
+
+        //D
+
+        public static void DeleteCustomer(int CustomerID)
+        {
+
+            string query = $"DELETE FROM Product WHERE ProductID = '{CustomerID}'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+        }
     }
 }
--- PigegruppenProjektSynsPunkt/ButiksSystem/Repository/ProductDatabaseSQL.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Repository/ProductDatabaseSQL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,76 @@ namespace ButiksSystem.Repository
 {
     internal class ProductDatabaseSQL
     {
+        //Vi giver systemet den connection string den skal bruge for at oprette forbindelse til databasen
+        private static SqlConnection connection = new SqlConnection("Insert connection string HERE!");
+
+        //C
+        public static void CreateProduct(int ProductID, string ProductName, double Price, int CategoryID, int Quantity)
+        {
+
+            string query = $"INSERT INTO Product (ProductID,ProductName,Price,CategoryID,Quantity) " +
+                $"VALUES ({ProductID.ToString()}, '{ProductName}', {Price.ToString().Replace(',', '.')}, {CategoryID.ToString()}, {Quantity.ToString()})";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+        }
+
+
+        //R
+        public static List<string> ReadProduct()
+        {
+            List<string> result = new List<string>();
+            string query = "SELECT * FROM Product";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader(); //Åbner readeren 
+
+            while (reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row += reader[i].ToString() + " - "; // Aflæser data og laver det om til string
+                }
+                result.Add(row); //tilføjer data til listen
+            }
+
+            reader.Close(); // lukker aflæseren og forbindelsen 
+            connection.Close();
+            return result; // retunerer resultatet som en liste
+
+        }
+
+        //U
+        public static void UpdateProduct(int ProductID, string ProductName, double Price, int CategoryID, int Quantity)
+        {
+            //Update sætningen herunder er muligvis ikke korrekt
+            string query = $"UPDATE Product SET ProductName = '{ProductName}', Price = '{Price}', CategoryID = '{CategoryID}', Quantity = '{Quantity}' WHERE ProductID = '{ProductID}'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+
+        }
+
+        //D
+
+        public static void DeleteProduct(int ProductID)
+        {
+
+            string query = $"DELETE FROM Product WHERE ProductID = '{ProductID}'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int count = command.ExecuteNonQuery(); //NonQuery betyder at vi ikke retunerer noget andet end i besked med at databasen er opdateret
+
+            connection.Close();
+        }
+
     }
 }
