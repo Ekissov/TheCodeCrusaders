63e1fbb1b51ec6efb2f08eff55bcc7865143ce3e
Emeli Kissov <eiek52989@edu.ucl.dk>
2023-05-22 20:07:14 +0200

    dummyDataList in DummydataSales updated

--- PigegruppenProjektSynsPunkt/ButiksSystem/Database/DummyDataSales.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Database/DummyDataSales.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ButiksSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,24 @@ namespace ButiksSystem.Database
 {
     public class DummyDataSales
     {
-        public int MyProperty { get; set; }
+
+        private static List<Sales> TestMethod()
+        {
+           List<Sales> listOfSales = new List<Sales>();
+
+            var orderDate = DateTime.Now.Date;
+            var deliveryDate = new DateTime(2023, 05, 29);
+
+            Sales sale1 = new Sales(1, 12, "1", orderDate, 4534, 4534, 1133.5M, deliveryDate, 1);
+
+            orderDate = new DateTime(2023, 01, 25);
+            deliveryDate = new DateTime(2023, 05, 29);
+
+            Sales sale2 = new Sales(2, 15, "12", orderDate, 4534, 4534, 1133.5M, deliveryDate, 1);
+
+            listOfSales.Add(sale1);
+            listOfSales.Add(sale2);
+            return listOfSales;
+        }
     }
 }
