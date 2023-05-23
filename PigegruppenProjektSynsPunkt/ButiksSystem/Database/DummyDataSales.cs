0e84b392f81cd83caa947ef734f68bb289c04d9b
Emeli Kissov <eiek52989@edu.ucl.dk>
2023-05-23 11:09:36 +0200

    Sales renamed to CostumerOrder

--- PigegruppenProjektSynsPunkt/ButiksSystem/Database/DummyDataSales.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/Database/DummyDataSales.cs
@@ -10,34 +10,34 @@ namespace ButiksSystem.Database
     public class DummyDataSales
     {
 
-        public static List<Sales> CreateDummyDataSales()
+        public static List<CostumerOrder> CreateDummyDataSales()
         {
-           List<Sales> listOfSales = new List<Sales>();
+           List<CostumerOrder> listOfSales = new List<CostumerOrder>();
 
             var orderDate = DateTime.Now.Date;
             var deliveryDate = new DateTime(2023, 05, 29);
 
-            Sales sale1 = new Sales(1, 12, "1", orderDate, 4534, deliveryDate, 1);
+            CostumerOrder sale1 = new CostumerOrder(1, 12, "1", orderDate, 4534, deliveryDate, 1);
 
             orderDate = new DateTime(2023, 01, 25);
             deliveryDate = new DateTime(2023, 02, 03);
 
-            Sales sale2 = new Sales(2, 15, "12", orderDate, 4534, deliveryDate, 4);
+            CostumerOrder sale2 = new CostumerOrder(2, 15, "12", orderDate, 4534, deliveryDate, 4);
             
             orderDate = new DateTime(2023, 02, 10);
             deliveryDate = new DateTime(2023, 02, 20);
 
-            Sales sale3 = new Sales(3, 8, "17", orderDate, 4000, deliveryDate, 2);
+            CostumerOrder sale3 = new CostumerOrder(3, 8, "17", orderDate, 4000, deliveryDate, 2);
 
             orderDate = new DateTime(2023, 01, 10);
             deliveryDate = new DateTime(2023, 01, 19);
 
-            Sales sale4 = new Sales(4, 11, "4", orderDate, 8000, deliveryDate, 3);
+            CostumerOrder sale4 = new CostumerOrder(4, 11, "4", orderDate, 8000, deliveryDate, 3);
 
             orderDate = new DateTime(2023, 03, 25);
             deliveryDate = new DateTime(2023, 04, 03);
 
-            Sales sale5 = new Sales(2, 5, "9", orderDate, 2500, deliveryDate, 1);
+            CostumerOrder sale5 = new CostumerOrder(2, 5, "9", orderDate, 2500, deliveryDate, 1);
 
             listOfSales.Add(sale1);
             listOfSales.Add(sale2);
