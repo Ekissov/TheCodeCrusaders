739f1684107e0b4ab5add0a38c5d3646728df88e
Emeli Kissov <eiek52989@edu.ucl.dk>
2023-05-24 12:03:48 +0200

    Salesstatisticsform updated

--- PigegruppenProjektSynsPunkt/ButiksSystem/UI/SalesStatisticForm.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/UI/SalesStatisticForm.cs
@@ -15,19 +15,40 @@ namespace ButiksSystem.UI
 {
     public partial class SalesStatisticForm : Form
     {
+        //Properties that is needed to store information
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public List<CostumerOrder> ListOfCostumerOrders { get; set; }
 
+        
         public SalesStatisticForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Shows all DummyData in datagridview, when the form is loaded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SalesStatistic_Load(object sender, EventArgs e)
+        {
+            dgv_showSalesStatisticsInDatagrid.DataSource = Database.DummyDataSales.CreateDummyDataSales();
+        }
+        /// <summary>
+        /// Sends the given enddate to be stored in the property StartDate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void dtp_salesStatisticsStartDate_ValueChanged(object sender, EventArgs e)
         {
             StartDate = dtp_salesStatisticsStartDate.Value;
         }
+        /// <summary>
+        /// Sends the given enddate to be stored in the property EndDate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void dtp_salesStatisticsEndDate_ValueChanged(object sender, EventArgs e)
         {
            EndDate = dtp_salesStatisticsEndDate.Value;
@@ -37,11 +58,6 @@ namespace ButiksSystem.UI
             UIController uIController = new UIController();
             ListOfCostumerOrders = uIController.GetCostumerOrdersWithinGivenTime(StartDate, EndDate);
             dgv_showSalesStatisticsInDatagrid.DataSource = ListOfCostumerOrders;
-        }
-
-        private void SalesStatistic_Load(object sender, EventArgs e)
-        {
-            dgv_showSalesStatisticsInDatagrid.DataSource = Database.DummyDataSales.CreateDummyDataSales();
         }
 
         private void btn_salesStatisticsPrintToTxtFile_Click(object sender, EventArgs e)
