e2ba1834807354e8003faa1049f72eb90614abc8
Linda_Agnete <lalj52999@edu.ucl.dk>
2023-05-25 12:52:59 +0200

    Har fået ProductID med på textfilen og hevet en metode fra services til UI laget

--- PigegruppenProjektSynsPunkt/ButiksSystem/UI/ShowAllProductsForm.Designer.cs
+++ PigegruppenProjektSynsPunkt/ButiksSystem/UI/ShowAllProductsForm.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dgv_showProductInfo = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_printAllProductsToTxtfile = new System.Windows.Forms.Button();
             this.btn_showAllProducts = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_showProductInfo)).BeginInit();
             this.SuspendLayout();
@@ -55,15 +55,15 @@
             this.dgv_showProductInfo.Size = new System.Drawing.Size(824, 396);
             this.dgv_showProductInfo.TabIndex = 37;
             // 
-            // button1
+            // btn_printAllProductsToTxtfile
             // 
-            this.button1.Location = new System.Drawing.Point(713, 523);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 50);
-            this.button1.TabIndex = 38;
-            this.button1.Text = "Print Alle Varer til txtfil.";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_printAllProductsToTxtfile.Location = new System.Drawing.Point(713, 523);
+            this.btn_printAllProductsToTxtfile.Name = "btn_printAllProductsToTxtfile";
+            this.btn_printAllProductsToTxtfile.Size = new System.Drawing.Size(194, 50);
+            this.btn_printAllProductsToTxtfile.TabIndex = 38;
+            this.btn_printAllProductsToTxtfile.Text = "Print Alle Varer til txtfil.";
+            this.btn_printAllProductsToTxtfile.UseVisualStyleBackColor = true;
+            this.btn_printAllProductsToTxtfile.Click += new System.EventHandler(this.button1_Click);
             // 
             // btn_showAllProducts
             // 
@@ -81,7 +81,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(972, 585);
             this.Controls.Add(this.btn_showAllProducts);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_printAllProductsToTxtfile);
             this.Controls.Add(this.dgv_showProductInfo);
             this.Controls.Add(this.label1);
             this.Name = "ShowAllProductsForm";
@@ -96,7 +96,7 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgv_showProductInfo;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_printAllProductsToTxtfile;
         private System.Windows.Forms.Button btn_showAllProducts;
     }
 }
